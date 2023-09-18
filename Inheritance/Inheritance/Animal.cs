475d8d18a1dbef879e1bddc9e5280d52fe09243a
Özay Melih Yıldız <ozay.m.yildiz@gmail.com>
2023-09-18 22:34:54 +0300

    last

---
+++ Inheritance/Inheritance/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Animal
+    {
+        public string animalName;
+        public string animalRace;
+        public string animalColour;
+
+        public void ShowAnimalInformation()
+        {
+            Console.WriteLine("Animal name = " +  animalName);
+            Console.WriteLine("Animal race = " + animalRace);
+            Console.WriteLine("Animal colour = " + animalColour);
+        }
+    }
+}
